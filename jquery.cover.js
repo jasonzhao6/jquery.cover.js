02d22ce62236f88a693968415274ded406c6c033
Jason Zhao <jasonzhao6@gmail.com>
2012-09-08 00:35:52 -0700

    refreshed demo page

--- jquery.cover.js
+++ jquery.cover.js
@@ -34,9 +34,9 @@
               'background-image': 'url(' + $image.attr('src') + ')',
               'background-position': imageAlign.h + ' ' + imageAlign.v
             });
-      		},
-      		jsSolutionForOlderBrowsers = function() {
-      		  // Adjust for horizontal alignment, default to left
+          },
+          jsSolutionForOlderBrowsers = function() {
+            // Adjust for horizontal alignment, default to left
             switch (imageAlign.h) {
               case 'center':
                 imageAlign.h = 2;
@@ -47,7 +47,7 @@
               default:
                 imageAlign.h = 1000000;
             }
-      		  // Adjust for vertical alignment, default to top
+            // Adjust for vertical alignment, default to top
             switch (imageAlign.v) {
               case 'center':
                 imageAlign.v = 2;
@@ -60,31 +60,31 @@
             }
             $(window).resize(resizeAction).trigger('resize');
             $image.show();
-      		},
-      		resizeAction = function() {
-      			windowSize = {
-      				w: $(window).width(),
-      				h: $(window).height()
-      			};
-      			windowAspectRatio = windowSize.w / windowSize.h;
-      			// When background image is wider than window
-      			if (imageAspectRatio > windowAspectRatio) {
+          },
+          resizeAction = function() {
+            windowSize = {
+              w: $(window).width(),
+              h: $(window).height()
+            };
+            windowAspectRatio = windowSize.w / windowSize.h;
+            // When background image is wider than window
+            if (imageAspectRatio > windowAspectRatio) {
               $image.css({
                 'left': Math.min(((windowSize.w - imageSize.w * windowSize.h / imageSize.h) / imageAlign.h), 0) + 'px',
                 'top': '0',
                 'width': 'auto',
                 'height': '100%'
               });
-      			// When window is wider than background image
-      			} else {
+            // When window is wider than background image
+            } else {
               $image.css({
                 'left': '0',
                 'top': Math.min(((windowSize.h - imageSize.h * windowSize.w / imageSize.w) / imageAlign.v), 0) + 'px',
                 'width': '100%',
                 'height': 'auto'
               });
-      			}
-      		};
+            }
+          };
       initialize();
     }
   });
